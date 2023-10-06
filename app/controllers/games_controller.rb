78d2e3665ed28b2867034ddc41f01c6056763ce9
otavio gomes <otaviojdggomes@gmail.com>
2023-10-05 21:05:01 -0300

    new game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,47 @@
+require 'nokogiri'
+require 'net/http'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      random_letter = ('a'..'z').to_a.sample
+      @letters << random_letter
+    end
+  end
+
+  def score
+    word = params[:word]
+    letters = params[:letters].split
+
+    valid_word = word.chars.all? { |letter| letters.include?(letter) }
+    english_word = check_english_word(word)
+
+    if valid_word
+      if english_word
+        @result = "Congratulations! #{word} is a valid word!"
+      else
+        @result = "Sorry, #{word} is not a valid English word!"
+      end
+    else
+      @result = "#{word} can't be built with #{letters.join(', ')}"
+    end
+  end
+
+  def check_english_word(word)
+    url = URI.parse("https://wagon-dictionary.herokuapp.com/#{word}")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == 'https')
+
+    request = Net::HTTP::Get.new(url.request_uri)
+
+    response = http.request(request)
+
+    if response.code == '200'
+      data = JSON.parse(response.body)
+      return data['found']
+    else
+      return false
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "new", to: "games#new"
+  post "score", to: "games#score"
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
